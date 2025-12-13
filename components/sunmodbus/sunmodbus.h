c698eca7ba1f68f407fb2cfd9d74ccc5ec9013a9
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-14 00:03:54 +0100

    Update sunmodbus.h

--- components/sunmodbus/sunmodbus.h
+++ components/sunmodbus/sunmodbus.h
@@ -7,45 +7,44 @@
 namespace esphome {
 namespace sunmodbus {
 
-enum class DataType {
+enum DataType {
   TYPE_UINT16 = 0,
   TYPE_INT16 = 1,
 };
 
 class SunModbus : public PollingComponent, public uart::UARTDevice {
  public:
-  SunModbus() : PollingComponent(1000) {}
+  SunModbus() = default;
+
+  void set_uart(uart::UARTComponent *uart) { this->uart_ = uart; }
+  void set_sensor(sensor::Sensor *sensor) { this->sensor_ = sensor; }
+
+  void set_slave_id(uint8_t slave_id) { this->slave_id_ = slave_id; }
+  void set_start_address(uint16_t start_address) { this->start_address_ = start_address; }
+  void set_count(uint16_t count) { this->count_ = count; }
+  void set_offset(uint16_t offset) { this->offset_ = offset; }
+  void set_scale(float scale) { this->scale_ = scale; }
+  void set_type(DataType type) { this->type_ = type; }
+
+  void set_update_interval(uint32_t update_interval) { this->set_update_interval_ms(update_interval); }
 
   void setup() override;
   void update() override;
-
-  void set_uart(uart::UARTComponent *uart) { this->parent_ = uart; }
-  void set_slave_id(uint8_t id) { this->slave_id_ = id; }
-  void set_start_address(uint16_t addr) { this->start_address_ = addr; }
-  void set_count(uint16_t count) { this->count_ = count; }
-
-  // ✅ DEN VIGTIGE RETTELSE
-  void set_update_interval(uint32_t ms) { this->PollingComponent::set_update_interval(ms); }
-  void set_sensor(sensor::Sensor *s) { this->sensor_ = s; }
-
-  void set_offset(uint16_t offset) { this->offset_ = offset; }
-  void set_type(DataType type) { this->type_ = type; }
-  void set_scale(float scale) { this->scale_ = scale; }
+  float get_setup_priority() const override { return setup_priority::DATA; }
 
  protected:
-  bool read_registers(uint16_t start, uint16_t count, uint16_t *buffer);
-
-  uart::UARTComponent *parent_{nullptr};
+  uart::UARTComponent *uart_{nullptr};
   sensor::Sensor *sensor_{nullptr};
 
   uint8_t slave_id_{1};
   uint16_t start_address_{0};
   uint16_t count_{1};
   uint16_t offset_{0};
-  DataType type_{DataType::TYPE_UINT16};
   float scale_{1.0f};
+  DataType type_{TYPE_UINT16};
+
+  bool read_holding_registers_(uint8_t slave, uint16_t start, uint16_t count, uint8_t *buffer, uint16_t len);
 };
 
 }  // namespace sunmodbus
 }  // namespace esphome
-
