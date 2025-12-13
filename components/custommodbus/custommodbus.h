acf14ad9dfae4259922d73e6ee0799b1413b3baf
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-13 17:47:40 +0100

    Update custommodbus.h

--- components/custommodbus/custommodbus.h
+++ components/custommodbus/custommodbus.h
@@ -1,20 +1,24 @@
 #pragma once
-#include "esphome.h"
+
+#include "esphome/core/component.h"
+#include "esphome/components/uart/uart.h"
+#include "esphome/components/sensor/sensor.h"
 
 namespace custommodbus {
 
-class CustomModbus : public esphome::Component, public esphome::UARTDevice {
+class CustomModbus : public esphome::Component, public esphome::uart::UARTDevice {
  public:
-  esphome::Sensor *sensor_ = new esphome::Sensor();
-
-  CustomModbus(esphome::UARTComponent *parent);
-
+  // Sensoren oprettes af Python-delen og injiceres via setter
+  void set_sensor(esphome::sensor::Sensor *sensor) { sensor_ = sensor; }
   void set_name(const std::string &name) { name_ = name; }
+
   void setup() override;
   void loop() override;
 
  protected:
   std::string name_;
+  esphome::sensor::Sensor *sensor_{nullptr};
+
   uint16_t crc16(uint8_t *buf, uint8_t len);
 };
 
