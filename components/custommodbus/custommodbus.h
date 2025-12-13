c1d55a93ec0e1c21e3f33ce4efbcfab9bef0caa7
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-13 18:27:48 +0100

    Update custommodbus.h

--- components/custommodbus/custommodbus.h
+++ components/custommodbus/custommodbus.h
@@ -6,12 +6,17 @@
 
 namespace custommodbus {
 
+enum DataType {
+  TYPE_UINT16 = 0,
+  TYPE_INT16 = 1,
+};
+
 class CustomModbus : public esphome::Component, public esphome::uart::UARTDevice {
  public:
   void set_slave_id(uint8_t id) { slave_id_ = id; }
   void set_register(uint16_t reg) { register_ = reg; }
   void set_count(uint8_t c) { count_ = c; }
-  void set_data_type(uint8_t t) { data_type_ = t; }
+  void set_data_type(DataType t) { data_type_ = t; }
   void set_scale(float s) { scale_ = s; }
   void set_sensor(esphome::sensor::Sensor *s) { sensor_ = s; }
 
@@ -22,7 +27,7 @@ class CustomModbus : public esphome::Component, public esphome::uart::UARTDevice
   uint8_t slave_id_{1};
   uint16_t register_{0};
   uint8_t count_{1};
-  uint8_t data_type_{0};
+  DataType data_type_{TYPE_UINT16};
   float scale_{1.0f};
   esphome::sensor::Sensor *sensor_{nullptr};
 
