759b663f1d19b18b7ae79fc48968aea0918ebbab
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-13 22:38:06 +0100

    Update sunmodbus.h

--- components/sunmodbus/sunmodbus.h
+++ components/sunmodbus/sunmodbus.h
@@ -7,9 +7,15 @@
 namespace esphome {
 namespace sunmodbus {
 
+// samme enum som Python refererer til
+enum class DataType {
+  TYPE_UINT16 = 0,
+  TYPE_INT16 = 1,
+};
+
 class SunModbus : public PollingComponent, public uart::UARTDevice {
  public:
-  SunModbus() : PollingComponent(1000) {}  // default 1s
+  SunModbus() : PollingComponent(1000) {}  // default 1s, overskrives af set_update_interval hvis du vil
 
   void setup() override;
   void update() override;
@@ -18,22 +24,24 @@ class SunModbus : public PollingComponent, public uart::UARTDevice {
   void set_slave_id(uint8_t id) { this->slave_id_ = id; }
   void set_start_address(uint16_t addr) { this->start_address_ = addr; }
   void set_count(uint16_t count) { this->count_ = count; }
-  void set_update_interval(uint32_t ms) { this->update_interval_ = ms; }
+  void set_update_interval(uint32_t ms) { this->set_update_interval_ms(ms); }
 
   void set_offset(uint16_t offset) { this->offset_ = offset; }
   void set_type(DataType type) { this->type_ = type; }
   void set_scale(float scale) { this->scale_ = scale; }
 
  protected:
-  uart::UARTComponent *parent_;
+  bool read_registers(uint16_t start, uint16_t count, uint16_t *buffer);
+
+  uart::UARTComponent *parent_{nullptr};
   sensor::Sensor *sensor_{nullptr};
 
-  uint8_t slave_id_;
-  uint16_t start_address_;
-  uint16_t count_;
-  uint16_t offset_;
-  DataType type_;
-  float scale_;
+  uint8_t slave_id_{1};
+  uint16_t start_address_{0};
+  uint16_t count_{1};
+  uint16_t offset_{0};
+  DataType type_{DataType::TYPE_UINT16};
+  float scale_{1.0f};
 };
 
 }  // namespace sunmodbus
