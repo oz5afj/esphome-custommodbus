e2eb0d385f0d07726d5c6d5d5e79c1c0137c8592
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-13 21:48:21 +0100

    Update custommodbus.h

--- components/sunmodbus/custommodbus.h
+++ components/sunmodbus/custommodbus.h
@@ -1,43 +1,44 @@
 #pragma once
-#include "esphome/components/uart/uart.h"
+
 #include "esphome/core/component.h"
+#include "esphome/components/uart/uart.h"
+#include "esphome/components/sensor/sensor.h"
 
 namespace esphome {
-namespace custommodbus {
+namespace sunmodbus {
 
 enum DataType {
   TYPE_UINT16,
   TYPE_INT16,
 };
 
-class CustomModbus : public Component, public uart::UARTDevice {
+class SunModbus : public Component, public uart::UARTDevice {
  public:
-  void set_slave_id(uint8_t id) { slave_id_ = id; }
-  void set_start_address(uint16_t addr) { start_address_ = addr; }
-  void set_count(uint16_t c) { count_ = c; }
-  void set_update_interval(uint32_t ms) { update_interval_ = ms; }
-
-  void add_sensor(sensor::Sensor *s, uint8_t offset, DataType type, float scale);
-
   void setup() override;
   void loop() override;
+  void update() override;
+
+  void set_uart(uart::UARTComponent *uart) { this->uart_ = uart; }
+  void set_slave_id(uint8_t id) { this->slave_id_ = id; }
+  void set_start_address(uint16_t addr) { this->start_address_ = addr; }
+  void set_count(uint16_t c) { this->count_ = c; }
+  void set_update_interval(uint32_t ms) { this->update_interval_ = ms; }
+
+  void add_sensor(sensor::Sensor *sens, uint16_t offset, DataType type, float scale);
 
  protected:
+  bool read_registers(uint16_t start, uint16_t count, uint16_t *buffer);
+
+  uart::UARTComponent *uart_;
+  sensor::Sensor *sensor_;
   uint8_t slave_id_;
   uint16_t start_address_;
   uint16_t count_;
+  uint16_t offset_;
+  DataType type_;
+  float scale_;
   uint32_t update_interval_;
-  uint32_t last_read_{0};
-
-  struct SensorEntry {
-    sensor::Sensor *sensor;
-    uint8_t offset;
-    DataType type;
-    float scale;
-  };
-
-  std::vector<SensorEntry> sensors_;
 };
 
-}  // namespace custommodbus
+}  // namespace sunmodbus
 }  // namespace esphome
