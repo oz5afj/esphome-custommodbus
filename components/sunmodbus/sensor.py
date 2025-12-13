c69644be2940638e301c9aa96d72444230748ca7
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-13 22:52:26 +0100

    Update sensor.py

--- components/sunmodbus/sensor.py
+++ components/sunmodbus/sensor.py
@@ -1,11 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart, sensor
-from esphome.const import (
-    CONF_ID,
-    CONF_UART_ID,
-    CONF_NAME,
-)
+from esphome.const import CONF_ID, CONF_UART_ID
 
 sunmodbus_ns = cg.esphome_ns.namespace("sunmodbus")
 SunModbus = sunmodbus_ns.class_("SunModbus", cg.PollingComponent, uart.UARTDevice)
@@ -21,35 +17,43 @@ CONF_OFFSET = "offset"
 CONF_SCALE = "scale"
 CONF_TYPE = "type"
 CONF_UPDATE_INTERVAL = "update_interval"
+CONF_SENSOR = "sensor"
 
-DATA_TYPE_ENUM = cv.enum({
-    "uint16": DataType.TYPE_UINT16,
-    "int16": DataType.TYPE_INT16,
-}, upper=False)
+DATA_TYPE_ENUM = cv.enum(
+    {
+        "uint16": DataType.TYPE_UINT16,
+        "int16": DataType.TYPE_INT16,
+    },
+    upper=False,
+)
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(SunModbus),
+SENSOR_SCHEMA = sensor.sensor_schema().extend({})
 
-    cv.Required(CONF_NAME): cv.string,
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(SunModbus),
 
-    cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
-    cv.Required(CONF_SLAVE_ID): cv.int_range(min=1, max=247),
-    cv.Required(CONF_START_ADDRESS): cv.int_range(min=0, max=65535),
-    cv.Required(CONF_COUNT): cv.int_range(min=1, max=125),
-    cv.Required(CONF_OFFSET): cv.int_range(min=0, max=255),
-    cv.Required(CONF_SCALE): cv.float_,
-    cv.Required(CONF_TYPE): DATA_TYPE_ENUM,
-    cv.Optional(CONF_UPDATE_INTERVAL, default="1s"): cv.positive_time_period_milliseconds,
-}).extend(cv.COMPONENT_SCHEMA)
+        cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
+        cv.Required(CONF_SLAVE_ID): cv.int_range(min=1, max=247),
+        cv.Required(CONF_START_ADDRESS): cv.int_range(min=0, max=65535),
+        cv.Required(CONF_COUNT): cv.int_range(min=1, max=125),
+        cv.Required(CONF_OFFSET): cv.int_range(min=0, max=255),
+        cv.Required(CONF_SCALE): cv.float_,
+        cv.Required(CONF_TYPE): DATA_TYPE_ENUM,
+        cv.Optional(CONF_UPDATE_INTERVAL, default="1s"): cv.positive_time_period_milliseconds,
+
+        cv.Required(CONF_SENSOR): SENSOR_SCHEMA,
+    }
+).extend(cv.COMPONENT_SCHEMA)
 
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    # ✅ Opret sensor-objektet direkte
-    sens = cg.new_Pvariable(cg.generate_id(), sensor.Sensor)
-    cg.add(sens.set_name(config[CONF_NAME]))
+    # Opret sensoren ud fra den indlejrede sensor-schema
+    sens_conf = config[CONF_SENSOR]
+    sens = await sensor.new_sensor(sens_conf)
     cg.add(var.set_sensor(sens))
 
     uart_comp = await cg.get_variable(config[CONF_UART_ID])
