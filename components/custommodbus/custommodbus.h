2aed4a02656b1e43085a06f949ad82e220a11e65
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-13 21:22:27 +0100

    Update custommodbus.h

--- components/custommodbus/custommodbus.h
+++ components/custommodbus/custommodbus.h
@@ -1,40 +1,43 @@
 #pragma once
-
-#include "esphome/core/component.h"
 #include "esphome/components/uart/uart.h"
-#include "esphome/components/sensor/sensor.h"
+#include "esphome/core/component.h"
 
+namespace esphome {
 namespace custommodbus {
 
 enum DataType {
-  TYPE_UINT16 = 0,
-  TYPE_INT16 = 1,
+  TYPE_UINT16,
+  TYPE_INT16,
 };
 
-class CustomModbus : public esphome::Component, public esphome::uart::UARTDevice {
+class CustomModbus : public Component, public uart::UARTDevice {
  public:
   void set_slave_id(uint8_t id) { slave_id_ = id; }
-  void set_register(uint16_t reg) { register_ = reg; }
-  void set_count(uint8_t c) { count_ = c; }
+  void set_start_address(uint16_t addr) { start_address_ = addr; }
+  void set_count(uint16_t c) { count_ = c; }
+  void set_update_interval(uint32_t ms) { update_interval_ = ms; }
 
-  // Accept int from ESPHome and cast to enum
-  void set_data_type(int t) { data_type_ = static_cast<DataType>(t); }
+  void add_sensor(sensor::Sensor *s, uint8_t offset, DataType type, float scale);
 
-  void set_scale(float s) { scale_ = s; }
-  void set_sensor(esphome::sensor::Sensor *s) { sensor_ = s; }
-
-  void setup() override {}
+  void setup() override;
   void loop() override;
 
  protected:
-  uint8_t slave_id_{1};
-  uint16_t register_{0};
-  uint8_t count_{1};
-  DataType data_type_{TYPE_UINT16};
-  float scale_{1.0f};
-  esphome::sensor::Sensor *sensor_{nullptr};
+  uint8_t slave_id_;
+  uint16_t start_address_;
+  uint16_t count_;
+  uint32_t update_interval_;
+  uint32_t last_read_{0};
 
-  uint16_t crc16(uint8_t *buf, uint8_t len);
+  struct SensorEntry {
+    sensor::Sensor *sensor;
+    uint8_t offset;
+    DataType type;
+    float scale;
+  };
+
+  std::vector<SensorEntry> sensors_;
 };
 
 }  // namespace custommodbus
+}  // namespace esphome
