2bd79b98cdb526f4960494eeddb77163ee60c75b
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-13 21:03:06 +0100

    Update sensor_block.py

--- components/custommodbus/sensor_block.py
+++ components/custommodbus/sensor_block.py
@@ -1,17 +1,25 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart, sensor
+from esphome.const import CONF_ID, CONF_NAME, CONF_UART_ID
 
+# Namespace for C++ klasserne
 custommodbus_ns = cg.esphome_ns.namespace("custommodbus")
-
 CustomModbusBlock = custommodbus_ns.class_("CustomModbusBlock", cg.Component, uart.UARTDevice)
 
 DataType = custommodbus_ns.enum("DataType")
 
+# Platformnavn
+PLATFORM = "custommodbus_block"
+
+# Dependencies
+DEPENDENCIES = ["uart"]
+AUTO_LOAD = ["custommodbus"]
+
+# Konfigurationsfelter
 CONF_SLAVE_ID = "slave_id"
 CONF_START_ADDRESS = "start_address"
 CONF_COUNT = "count"
-CONF_SENSORS = "sensors"
 CONF_OFFSET = "offset"
 CONF_SCALE = "scale"
 CONF_TYPE = "type"
@@ -22,21 +30,17 @@ DATA_TYPE_ENUM = cv.enum({
     "int16": DataType.TYPE_INT16,
 }, upper=False)
 
-
-BLOCK_SENSOR_SCHEMA = sensor.sensor_schema().extend({
-    cv.Required(CONF_OFFSET): cv.int_range(min=0, max=255),
-    cv.Required(CONF_SCALE): cv.float_,
-    cv.Required(CONF_TYPE): DATA_TYPE_ENUM,
-})
-
-CONFIG_SCHEMA = cv.Schema({
+# Sensor-platform schema
+CONFIG_SCHEMA = sensor.sensor_schema(platform=PLATFORM).extend({
     cv.GenerateID(): cv.declare_id(CustomModbusBlock),
     cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
     cv.Required(CONF_SLAVE_ID): cv.int_range(min=1, max=247),
     cv.Required(CONF_START_ADDRESS): cv.int_range(min=0, max=65535),
     cv.Required(CONF_COUNT): cv.int_range(min=1, max=125),
+    cv.Required(CONF_OFFSET): cv.int_range(min=0, max=255),
+    cv.Required(CONF_SCALE): cv.float_,
+    cv.Required(CONF_TYPE): DATA_TYPE_ENUM,
     cv.Optional(CONF_UPDATE_INTERVAL, default="1s"): cv.positive_time_period_milliseconds,
-    cv.Required(CONF_SENSORS): cv.ensure_list(BLOCK_SENSOR_SCHEMA),
 }).extend(cv.COMPONENT_SCHEMA)
 
 
@@ -51,11 +55,13 @@ async def to_code(config):
     cg.add(var.set_count(config[CONF_COUNT]))
     cg.add(var.set_update_interval(config[CONF_UPDATE_INTERVAL]))
 
-    for s_conf in config[CONF_SENSORS]:
-        s = await sensor.new_sensor(s_conf)
-        cg.add(var.add_sensor(
-            s,
-            s_conf[CONF_OFFSET],
-            s_conf[CONF_TYPE],
-            s_conf[CONF_SCALE],
-        ))
+    sens = await sensor.new_sensor(config)
+    cg.add(var.add_sensor(
+        sens,
+        config[CONF_OFFSET],
+        config[CONF_TYPE],
+        config[CONF_SCALE],
+    ))
+
+
+CODEOWNERS = ["@oz5afj"]
