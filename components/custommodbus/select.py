8fbe6f8e65c7d0fda75b5cebbe64d3b76e7e5b78
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-14 15:18:18 +0100

    Update select.py

--- components/custommodbus/select.py
+++ components/custommodbus/select.py
@@ -3,33 +3,53 @@ import esphome.config_validation as cv
 from esphome.components import uart, select
 from esphome.const import CONF_ID
 
-custommodbus_ns = cg.esphome_ns.namespace("custommodbus")
-CustomModbus = custommodbus_ns.class_("CustomModbus", cg.Component, uart.UARTDevice)
+DEPENDENCIES = ["uart"]
 
+# Namespace
+custommodbus_ns = cg.esphome_ns.namespace("custommodbus")
+CustomModbusSelect = custommodbus_ns.class_(
+    "CustomModbusSelect", cg.Component, select.Select, uart.UARTDevice
+)
+
+# Konfigurationsnøgler
+CONF_SLAVE_ID = "slave_id"
+CONF_REGISTER = "register"
+CONF_OPTIONS = "options"
+
+# Schema for select-platformen
 CONFIG_SCHEMA = (
-    select.select_schema(CustomModbus)
+    select.select_schema(CustomModbusSelect)
     .extend(
         {
-            cv.GenerateID(): cv.declare_id(CustomModbus),
-            cv.Required("slave_id"): cv.int_range(min=1, max=247),
-            cv.Required("register"): cv.hex_uint16_t,
-            cv.Required("options"): cv.Schema({cv.string: cv.uint16_t}),
+            cv.GenerateID(): cv.declare_id(CustomModbusSelect),
+            cv.Required(CONF_SLAVE_ID): cv.int_range(min=1, max=247),
+            cv.Required(CONF_REGISTER): cv.hex_uint16_t,
+            cv.Required(CONF_OPTIONS): cv.Schema({cv.string: cv.uint16_t}),
         }
     )
     .extend(uart.UART_DEVICE_SCHEMA)
     .extend(cv.COMPONENT_SCHEMA)
 )
 
+# to_code-funktion med korrekt UART-håndtering
 async def to_code(config):
+    # Opret komponenten
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await uart.register_uart_device(var, config)
 
+    # Hent UART-objektet korrekt
+    uart_obj = await cg.get_variable(config["uart_id"])
+    cg.add(var.set_uart(uart_obj))
+
+    # Sæt slave_id
+    cg.add(var.set_slave_id(config[CONF_SLAVE_ID]))
+
+    # Opret select
     sel = await select.new_select(config)
 
-    cg.add(var.set_slave_id(config["slave_id"]))
-
-    for name, value in config["options"].items():
+    # Tilføj options
+    for name, value in config[CONF_OPTIONS].items():
         cg.add(sel.add_option(name, value))
 
-    cg.add(sel.add_on_state_callback(var.write_single(config["register"], sel)))
+    # Tilføj callback når state ændres
+    cg.add(sel.add_on_state_callback(var.write_single(config[CONF_REGISTER], sel)))
