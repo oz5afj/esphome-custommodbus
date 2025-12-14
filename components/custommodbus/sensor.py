354015971c2cbf38dcf929a3da0bbc561bb53d3b
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-14 15:05:07 +0100

    Update sensor.py

--- components/custommodbus/sensor.py
+++ components/custommodbus/sensor.py
@@ -1,36 +1,59 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome.components import sensor, uart
 from esphome.const import (
+    CONF_ID,
     CONF_NAME,
     CONF_UNIT_OF_MEASUREMENT,
     CONF_ACCURACY_DECIMALS,
+    CONF_UPDATE_INTERVAL,
 )
-
-from . import CustomModbus
 
 DEPENDENCIES = ["uart"]
 
-# Dette er en simpel sensor-klasse
-custommodbus_sensor_ns = cg.esphome_ns.namespace("custommodbus_sensor")
-CustomModbusSensor = custommodbus_sensor_ns.class_("CustomModbusSensor", cg.PollingComponent, cg.Sensor)
+# Namespace for vores component
+custommodbus_ns = cg.esphome_ns.namespace("custommodbus")
+CustomModbusSensor = custommodbus_ns.class_(
+    "CustomModbusSensor",
+    cg.PollingComponent,
+    sensor.Sensor,
+)
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(CustomModbusSensor),
-    cv.Required("uart_id"): cv.use_id(esphome.components.uart.UART),
-    cv.Required("slave_id"): cv.int_,
-    cv.Required("register"): cv.int_,
-    cv.Optional(CONF_UNIT_OF_MEASUREMENT): cv.string,
-    cv.Optional(CONF_ACCURACY_DECIMALS): cv.int_,
-}).extend(cg.POLLING_COMPONENT_SCHEMA)
+# Konfigurationsnøgler
+CONF_UART_ID = "uart_id"
+CONF_SLAVE_ID = "slave_id"
+CONF_REGISTER = "register"
+CONF_SCALE = "scale"
+
+# Schema for sensoren
+CONFIG_SCHEMA = (
+    sensor.sensor_schema(CustomModbusSensor)
+    .extend(
+        {
+            cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
+            cv.Required(CONF_SLAVE_ID): cv.uint8_t,
+            cv.Required(CONF_REGISTER): cv.uint16_t,
+            cv.Optional(CONF_SCALE, default=1.0): cv.float_,
+        }
+    )
+    .extend(cv.polling_component_schema("10s"))
+)
 
 async def to_code(config):
-    sensor = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(sensor, config)
-    await cg.add(sensor.set_uart_id(config["uart_id"]))
-    await cg.add(sensor.set_slave_id(config["slave_id"]))
-    await cg.add(sensor.set_register(config["register"]))
+    # Opret sensoren
+    var = await sensor.new_sensor(config)
+    
+    # Registrer componenten
+    await cg.register_component(var, config)
 
+    # Sæt parametre
+    cg.add(var.set_uart_id(config[CONF_UART_ID]))
+    cg.add(var.set_slave_id(config[CONF_SLAVE_ID]))
+    cg.add(var.set_register(config[CONF_REGISTER]))
+    cg.add(var.set_scale(config[CONF_SCALE]))
+
+    # Sæt unit og decimaler hvis de er defineret
     if CONF_UNIT_OF_MEASUREMENT in config:
-        cg.add(sensor.set_unit_of_measurement(config[CONF_UNIT_OF_MEASUREMENT]))
+        cg.add(var.set_unit_of_measurement(config[CONF_UNIT_OF_MEASUREMENT]))
     if CONF_ACCURACY_DECIMALS in config:
-        cg.add(sensor.set_accuracy_decimals(config[CONF_ACCURACY_DECIMALS]))
+        cg.add(var.set_accuracy_decimals(config[CONF_ACCURACY_DECIMALS]))
