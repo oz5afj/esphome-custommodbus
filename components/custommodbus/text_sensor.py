c5fe87600b7c507394f15b449aae8268d016ba0b
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-14 15:54:54 +0100

    Update text_sensor.py

--- components/custommodbus/text_sensor.py
+++ components/custommodbus/text_sensor.py
@@ -1,4 +1,4 @@
-iimport esphome.codegen as cg
+import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart, text_sensor
 from esphome.const import CONF_ID
@@ -6,7 +6,7 @@ from esphome.const import CONF_ID
 custommodbus_ns = cg.esphome_ns.namespace("custommodbus")
 CustomModbus = custommodbus_ns.class_("CustomModbus", cg.Component, uart.UARTDevice)
 
-PLATFORM_SCHEMA = text_sensor.TEXT_SENSOR_PLATFORM_SCHEMA.extend(
+PLATFORM_SCHEMA = text_sensor.text_sensor_schema(CustomModbus).extend(
     {
         cv.Required(CONF_ID): cv.use_id(CustomModbus),
         cv.Required("slave_id"): cv.int_range(min=1, max=247),
