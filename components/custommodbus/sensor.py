e68a066249c40363b95e2f9e72bb631e851632d7
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-14 18:42:47 +0100

    Update sensor.py

--- components/custommodbus/sensor.py
+++ components/custommodbus/sensor.py
@@ -1,89 +1,45 @@
 import esphome.codegen as cg
-
 import esphome.config_validation as cv
-
 from esphome.components import uart, sensor
-
 from esphome.const import CONF_ID
 
-
-
 custommodbus_ns = cg.esphome_ns.namespace("custommodbus")
-
 CustomModbus = custommodbus_ns.class_("CustomModbus", cg.Component, uart.UARTDevice)
 
+PLATFORM_SCHEMA = sensor.sensor_schema().extend(
+    {
+        cv.GenerateID(): cv.declare_id(sensor.Sensor),
+        cv.Required("custommodbus_id"): cv.use_id(CustomModbus),
+        cv.Required("slave_id"): cv.int_range(min=1, max=247),
+        cv.Required("register"): cv.hex_uint16_t,
+        cv.Optional("count", default=1): cv.int_range(min=1, max=2),
+        cv.Optional("scale", default=1.0): cv.float_,
+        cv.Optional("data_type", default="uint16"): cv.string,
+    }
+).extend(uart.UART_DEVICE_SCHEMA)
 
 
-DATA_TYPES = {
-
-
-    "uint16": cg.uint16,
-
-
-    "int16": cg.int16,
-
+DATA_TYPE_MAP = {
+    "uint16": 0,
+    "int16": 1,
+    "uint32": 2,
+    "uint32_r": 3,
 }
 
-
-
-CONFIG_SCHEMA = (
-
-    cv.Schema(
-
-        {
-
-            cv.GenerateID(CONF_ID): cv.declare_id(CustomModbus),
-
-
-
-            cv.Required("slave_id"): cv.int_range(min=1, max=247),
-
-            cv.Required("register"): cv.hex_uint16_t,
-
-            cv.Optional("count", default=1): cv.int_range(min=1, max=2),
-
-            cv.Optional("data_type", default="uint16"): cv.one_of(*DATA_TYPES.keys(), lower=True),
-
-            cv.Optional("scale", default=1.0): cv.float_,
-
-
-
-            cv.Required("sensor"): sensor.sensor_schema(),
-
-        }
-
-    )
-
-    .extend(uart.UART_DEVICE_SCHEMA)
-
-    .extend(cv.COMPONENT_SCHEMA)
-
-)
-
-
-
 async def to_code(config):
+    parent = await cg.get_variable(config[CONF_ID])
+    sens = await sensor.new_sensor(config)
 
-    var = cg.new_Pvariable(config[CONF_ID])
+    data_type = DATA_TYPE_MAP.get(config["data_type"], 0)
 
-    await cg.register_component(var, config)
+    cg.add(parent.set_slave_id(config["slave_id"]))
+    cg.add(parent.add_read_sensor(
+        config["register"],
+        config["count"],
+        data_type,
+        config["scale"],
+        sens
+    ))
 
-    await uart.register_uart_device(var, config)
+CONFIG_SCHEMA = PLATFORM_SCHEMA
 
-
-
-    cg.add(var.set_slave_id(config["slave_id"]))
-
-    cg.add(var.set_register(config["register"]))
-
-    cg.add(var.set_count(config["count"]))
-
-    cg.add(var.set_data_type(DATA_TYPES[config["data_type"]]))
-
-    cg.add(var.set_scale(config["scale"]))
-
-
-
-    sens = await sensor.new_sensor(config["sensor"])
-
-    cg.add(var.set_sensor(sens))
