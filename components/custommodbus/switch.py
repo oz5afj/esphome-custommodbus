4ab31db4b880ee496502a4b2e5cef75793c280f4
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-14 17:16:29 +0100

    Update switch.py

--- components/custommodbus/switch.py
+++ components/custommodbus/switch.py
@@ -6,8 +6,9 @@ from esphome.const import CONF_ID
 custommodbus_ns = cg.esphome_ns.namespace("custommodbus")
 CustomModbus = custommodbus_ns.class_("CustomModbus", cg.Component, uart.UARTDevice)
 
-PLATFORM_SCHEMA = switch.switch_schema(CustomModbus).extend(
+PLATFORM_SCHEMA = switch.switch_schema().extend(
     {
+        cv.GenerateID(): cv.declare_id(switch.Switch),
         cv.Required(CONF_ID): cv.use_id(CustomModbus),
         cv.Required("slave_id"): cv.int_range(min=1, max=247),
         cv.Required("register"): cv.hex_uint16_t,
@@ -16,14 +17,15 @@ PLATFORM_SCHEMA = switch.switch_schema(CustomModbus).extend(
 ).extend(uart.UART_DEVICE_SCHEMA)
 
 async def to_code(config):
-    var = cg.get_variable(config[CONF_ID])
-    await uart.register_uart_device(var, config)
+    parent = await cg.get_variable(config[CONF_ID])
+    await uart.register_uart_device(parent, config)
     sw = await switch.new_switch(config)
-    cg.add(var.set_slave_id(config["slave_id"]))
+
+    cg.add(parent.set_slave_id(config["slave_id"]))
 
     if "bitmask" in config:
-        cg.add(sw.add_on_turn_on(var.write_bitmask(config["register"], config["bitmask"], True)))
-        cg.add(sw.add_on_turn_off(var.write_bitmask(config["register"], config["bitmask"], False)))
+        cg.add(sw.add_on_turn_on(parent.write_bitmask(config["register"], config["bitmask"], True)))
+        cg.add(sw.add_on_turn_off(parent.write_bitmask(config["register"], config["bitmask"], False)))
     else:
-        cg.add(sw.add_on_turn_on(var.write_single(config["register"], 1)))
-        cg.add(sw.add_on_turn_off(var.write_single(config["register"], 0)))
+        cg.add(sw.add_on_turn_on(parent.write_single(config["register"], 1)))
+        cg.add(sw.add_on_turn_off(parent.write_single(config["register"], 0)))
