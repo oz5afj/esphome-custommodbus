0ddbdf9d9c89981425dfe34d146fd2d58512818e
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-14 16:11:28 +0100

    Update __init__.py

--- components/custommodbus/__init__.py
+++ components/custommodbus/__init__.py
@@ -1,6 +1,20 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+
 from . import sensor
 from . import binary_sensor
 from . import switch
 from . import number
 from . import select
 from . import text_sensor
+
+custommodbus_ns = cg.esphome_ns.namespace("custommodbus")
+CustomModbus = custommodbus_ns.class_("CustomModbus", cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(CustomModbus),
+})
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[cv.CONF_ID])
+    await cg.register_component(var, config)
