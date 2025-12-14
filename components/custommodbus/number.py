75976246d714342517d6942b2078015027ac87cc
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-14 17:18:06 +0100

    Update number.py

--- components/custommodbus/number.py
+++ components/custommodbus/number.py
@@ -6,8 +6,9 @@ from esphome.const import CONF_ID
 custommodbus_ns = cg.esphome_ns.namespace("custommodbus")
 CustomModbus = custommodbus_ns.class_("CustomModbus", cg.Component, uart.UARTDevice)
 
-PLATFORM_SCHEMA = number.number_schema(CustomModbus).extend(
+PLATFORM_SCHEMA = number.number_schema().extend(
     {
+        cv.GenerateID(): cv.declare_id(number.Number),
         cv.Required(CONF_ID): cv.use_id(CustomModbus),
         cv.Required("slave_id"): cv.int_range(min=1, max=247),
         cv.Required("register"): cv.hex_uint16_t,
@@ -15,8 +16,11 @@ PLATFORM_SCHEMA = number.number_schema(CustomModbus).extend(
 ).extend(uart.UART_DEVICE_SCHEMA)
 
 async def to_code(config):
-    var = cg.get_variable(config[CONF_ID])
-    await uart.register_uart_device(var, config)
+    parent = await cg.get_variable(config[CONF_ID])
+    await uart.register_uart_device(parent, config)
     num = await number.new_number(config)
-    cg.add(var.set_slave_id(config["slave_id"]))
-    cg.add(num.add_on_state_callback(var.write_single(config["register"], cg.float_(num))))
+
+    cg.add(parent.set_slave_id(config["slave_id"]))
+    cg.add(num.add_on_state_callback(
+        parent.write_single(config["register"], cg.float_(num))
+    ))
