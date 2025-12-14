8cef16416bb0302d76d57fce5c3191efc7f466d5
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-14 13:53:30 +0100

    Update custommodbus.h - Version 2

--- components/custommodbus/custommodbus.h
+++ components/custommodbus/custommodbus.h
@@ -3,39 +3,70 @@
 #include "esphome/core/component.h"
 #include "esphome/components/uart/uart.h"
 #include "esphome/components/sensor/sensor.h"
+#include "esphome/components/switch/switch.h"
+#include "esphome/components/number/number.h"
+#include "esphome/components/select/select.h"
+#include "esphome/components/binary_sensor/binary_sensor.h"
+#include "esphome/components/text_sensor/text_sensor.h"
 
 namespace custommodbus {
 
 enum DataType {
   TYPE_UINT16 = 0,
   TYPE_INT16 = 1,
+  TYPE_UINT32 = 2,
+  TYPE_UINT32_R = 3,
+};
+
+struct ReadItem {
+  uint16_t reg;
+  uint8_t count;
+  DataType type;
+  float scale;
+  esphome::sensor::Sensor *sensor;
+  esphome::binary_sensor::BinarySensor *binary_sensor;
+  uint16_t bitmask;
+  esphome::text_sensor::TextSensor *text_sensor;
+};
+
+struct WriteItem {
+  uint16_t reg;
+  uint16_t value;
+  uint16_t mask;
+  bool use_mask;
 };
 
 class CustomModbus : public esphome::Component, public esphome::uart::UARTDevice {
  public:
   void set_slave_id(uint8_t id) { slave_id_ = id; }
-  void set_register(uint16_t reg) { register_ = reg; }
-  void set_count(uint8_t c) { count_ = c; }
 
-  // Accept int from ESPHome and cast to enum
-  void set_data_type(int t) { data_type_ = static_cast<DataType>(t); }
+  // READ
+  void add_read_sensor(uint16_t reg, uint8_t count, DataType type, float scale,
+                       esphome::sensor::Sensor *s);
 
-  void set_scale(float s) { scale_ = s; }
-  void set_sensor(esphome::sensor::Sensor *s) { sensor_ = s; }
+  void add_binary_sensor(uint16_t reg, uint16_t mask,
+                         esphome::binary_sensor::BinarySensor *bs);
+
+  void add_text_sensor(uint16_t reg, esphome::text_sensor::TextSensor *ts);
+
+  // WRITE
+  void write_single(uint16_t reg, uint16_t value);
+  void write_bitmask(uint16_t reg, uint16_t mask, bool state);
 
   void setup() override {}
   void loop() override;
 
  protected:
   uint8_t slave_id_{1};
-  uint16_t register_{0};
-  uint8_t count_{1};
-  DataType data_type_{TYPE_UINT16};
-  float scale_{1.0f};
-  esphome::sensor::Sensor *sensor_{nullptr};
 
+  std::vector<ReadItem> reads_;
+  std::vector<WriteItem> writes_;
+
+  void process_reads();
+  void process_writes();
+
+  bool read_registers(uint16_t reg, uint8_t count, uint8_t *resp, uint8_t &resp_len);
   uint16_t crc16(uint8_t *buf, uint8_t len);
 };
 
 }  // namespace custommodbus
-
