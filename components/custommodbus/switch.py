f1e6e1a7f11d4c930c1c17ad23fa0303a774e3c2
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-14 15:42:34 +0100

    Update switch.py

--- components/custommodbus/switch.py
+++ components/custommodbus/switch.py
@@ -3,55 +3,27 @@ import esphome.config_validation as cv
 from esphome.components import uart, switch
 from esphome.const import CONF_ID
 
-DEPENDENCIES = ["uart"]
-
-# Namespace
 custommodbus_ns = cg.esphome_ns.namespace("custommodbus")
-CustomModbusSwitch = custommodbus_ns.class_(
-    "CustomModbusSwitch", cg.Component, switch.Switch, uart.UARTDevice
-)
+CustomModbus = custommodbus_ns.class_("CustomModbus", cg.Component, uart.UARTDevice)
 
-# Konfigurationsnøgler
-CONF_SLAVE_ID = "slave_id"
-CONF_REGISTER = "register"
-CONF_BITMASK = "bitmask"
+PLATFORM_SCHEMA = switch.SWITCH_PLATFORM_SCHEMA.extend(
+    {
+        cv.Required(CONF_ID): cv.use_id(CustomModbus),
+        cv.Required("slave_id"): cv.int_range(min=1, max=247),
+        cv.Required("register"): cv.hex_uint16_t,
+        cv.Optional("bitmask"): cv.hex_uint16_t,
+    }
+).extend(uart.UART_DEVICE_SCHEMA)
 
-# Schema for switch-platformen
-CONFIG_SCHEMA = (
-    switch.switch_schema(CustomModbusSwitch)
-    .extend(
-        {
-            cv.GenerateID(): cv.declare_id(CustomModbusSwitch),
-            cv.Required(CONF_SLAVE_ID): cv.int_range(min=1, max=247),
-            cv.Required(CONF_REGISTER): cv.hex_uint16_t,
-            cv.Optional(CONF_BITMASK): cv.hex_uint16_t,
-        }
-    )
-    .extend(uart.UART_DEVICE_SCHEMA)
-    .extend(cv.COMPONENT_SCHEMA)
-)
-
-# to_code-funktion med korrekt async/variable-håndtering
 async def to_code(config):
-    # Opret switch-componenten
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-
-    # Hent UART-objektet korrekt
-    uart_obj = await cg.get_variable(config["uart_id"])
-    cg.add(var.set_uart(uart_obj))
-
-    # Sæt slave_id og register
-    cg.add(var.set_slave_id(config[CONF_SLAVE_ID]))
-    cg.add(var.set_register(config[CONF_REGISTER]))
-
-    # Opret selve switchen
+    var = cg.get_variable(config[CONF_ID])
+    await uart.register_uart_device(var, config)
     sw = await switch.new_switch(config)
+    cg.add(var.set_slave_id(config["slave_id"]))
 
-    # Tilføj on/off-handling
-    if CONF_BITMASK in config:
-        cg.add(sw.add_on_turn_on(var.write_bitmask(config[CONF_REGISTER], config[CONF_BITMASK], True)))
-        cg.add(sw.add_on_turn_off(var.write_bitmask(config[CONF_REGISTER], config[CONF_BITMASK], False)))
+    if "bitmask" in config:
+        cg.add(sw.add_on_turn_on(var.write_bitmask(config["register"], config["bitmask"], True)))
+        cg.add(sw.add_on_turn_off(var.write_bitmask(config["register"], config["bitmask"], False)))
     else:
-        cg.add(sw.add_on_turn_on(var.write_single(config[CONF_REGISTER], 1)))
-        cg.add(sw.add_on_turn_off(var.write_single(config[CONF_REGISTER], 0)))
+        cg.add(sw.add_on_turn_on(var.write_single(config["register"], 1)))
+        cg.add(sw.add_on_turn_off(var.write_single(config["register"], 0)))
