1f7254e2c038cf5f3e005f23bba95ed895aebe8b
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-14 14:17:16 +0100

    Update binary_sensor.py

--- components/custommodbus/binary_sensor.py
+++ components/custommodbus/binary_sensor.py
@@ -7,7 +7,7 @@ custommodbus_ns = cg.esphome_ns.namespace("custommodbus")
 CustomModbus = custommodbus_ns.class_("CustomModbus", cg.Component, uart.UARTDevice)
 
 CONFIG_SCHEMA = (
-    binary_sensor.binary_sensor_schema()
+    binary_sensor.binary_sensor_schema(CustomModbus)
     .extend(
         {
             cv.GenerateID(): cv.declare_id(CustomModbus),
