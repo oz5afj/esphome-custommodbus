9350c793500facd45143cc99cb84d7e41bcbf27d
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-14 15:55:42 +0100

    Update switch.py

--- components/custommodbus/switch.py
+++ components/custommodbus/switch.py
@@ -6,7 +6,7 @@ from esphome.const import CONF_ID
 custommodbus_ns = cg.esphome_ns.namespace("custommodbus")
 CustomModbus = custommodbus_ns.class_("CustomModbus", cg.Component, uart.UARTDevice)
 
-PLATFORM_SCHEMA = switch.SWITCH_PLATFORM_SCHEMA.extend(
+PLATFORM_SCHEMA = switch.switch_schema(CustomModbus).extend(
     {
         cv.Required(CONF_ID): cv.use_id(CustomModbus),
         cv.Required("slave_id"): cv.int_range(min=1, max=247),
