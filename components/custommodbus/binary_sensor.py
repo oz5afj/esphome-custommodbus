52458c5429095da69a8cb038b8a90ee71d83e269
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-14 17:18:45 +0100

    Update binary_sensor.py

--- components/custommodbus/binary_sensor.py
+++ components/custommodbus/binary_sensor.py
@@ -6,8 +6,9 @@ from esphome.const import CONF_ID
 custommodbus_ns = cg.esphome_ns.namespace("custommodbus")
 CustomModbus = custommodbus_ns.class_("CustomModbus", cg.Component, uart.UARTDevice)
 
-PLATFORM_SCHEMA = binary_sensor.binary_sensor_schema(CustomModbus).extend(
+PLATFORM_SCHEMA = binary_sensor.binary_sensor_schema().extend(
     {
+        cv.GenerateID(): cv.declare_id(binary_sensor.BinarySensor),
         cv.Required(CONF_ID): cv.use_id(CustomModbus),
         cv.Required("slave_id"): cv.int_range(min=1, max=247),
         cv.Required("register"): cv.hex_uint16_t,
@@ -16,8 +17,13 @@ PLATFORM_SCHEMA = binary_sensor.binary_sensor_schema(CustomModbus).extend(
 ).extend(uart.UART_DEVICE_SCHEMA)
 
 async def to_code(config):
-    var = cg.get_variable(config[CONF_ID])
-    await uart.register_uart_device(var, config)
+    parent = await cg.get_variable(config[CONF_ID])
+    await uart.register_uart_device(parent, config)
     bs = await binary_sensor.new_binary_sensor(config)
-    cg.add(var.set_slave_id(config["slave_id"]))
-    cg.add(var.add_binary_sensor(config["register"], config["bitmask"], bs))
+
+    cg.add(parent.set_slave_id(config["slave_id"]))
+    cg.add(parent.add_binary_sensor(
+        config["register"],
+        config["bitmask"],
+        bs
+    ))
