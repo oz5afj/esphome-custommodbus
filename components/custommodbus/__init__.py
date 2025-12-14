aabd7811cdab16eb2eccce1b34c928fbdada2f57
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-14 16:57:12 +0100

    Update __init__.py

--- components/custommodbus/__init__.py
+++ components/custommodbus/__init__.py
@@ -4,6 +4,8 @@ import esphome.config_validation as cv
 custommodbus_ns = cg.esphome_ns.namespace("custommodbus")
 CustomModbus = custommodbus_ns.class_("CustomModbus", cg.Component)
 
+AUTO_LOAD = ["sensor", "binary_sensor", "switch", "number", "select", "text_sensor"]
+
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(CustomModbus),
 })
