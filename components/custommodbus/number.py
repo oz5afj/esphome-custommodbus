3ed42adefa47a4646688986f26dcf53ac63b59e3
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-14 14:00:10 +0100

    Create number.py

---
+++ components/custommodbus/number.py
@@ -0,0 +1,31 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import uart, number
+from esphome.const import CONF_ID
+
+custommodbus_ns = cg.esphome_ns.namespace("custommodbus")
+CustomModbus = custommodbus_ns.class_("CustomModbus", cg.Component, uart.UARTDevice)
+
+CONFIG_SCHEMA = (
+    number.number_schema()
+    .extend(
+        {
+            cv.GenerateID(): cv.declare_id(CustomModbus),
+            cv.Required("slave_id"): cv.int_range(min=1, max=247),
+            cv.Required("register"): cv.hex_uint16_t,
+        }
+    )
+    .extend(uart.UART_DEVICE_SCHEMA)
+    .extend(cv.COMPONENT_SCHEMA)
+)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await uart.register_uart_device(var, config)
+
+    num = await number.new_number(config)
+
+    cg.add(var.set_slave_id(config["slave_id"]))
+
+    cg.add(num.add_on_state_callback(var.write_single(config["register"], cg.float_(num))))
