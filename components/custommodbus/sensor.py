9c433ad31adc3f054d5764dc398674f34ba47cd2
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-15 18:09:46 +0100

    Update sensor.py

--- components/custommodbus/sensor.py
+++ components/custommodbus/sensor.py
@@ -1,44 +1,62 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import uart, sensor
-from esphome.const import CONF_ID
+from esphome.components import sensor
+from esphome.const import (
+    CONF_ID,
+    CONF_REGISTER,
+    CONF_COUNT,
+    CONF_DATA_TYPE,
+    CONF_SCALE,
+)
 
+# Namespace matcher C++: esphome::custommodbus
 custommodbus_ns = cg.esphome_ns.namespace("custommodbus")
-CustomModbus = custommodbus_ns.class_("CustomModbus", cg.Component, uart.UARTDevice)
 
-PLATFORM_SCHEMA = sensor.sensor_schema().extend(
-    {
-        cv.Required("custommodbus_id"): cv.use_id(CustomModbus),
-        cv.Required("slave_id"): cv.int_range(min=1, max=247),
-        cv.Required("register"): cv.hex_uint16_t,
-        cv.Optional("count", default=1): cv.int_range(min=1, max=2),
-        cv.Optional("scale", default=1.0): cv.float_,
-        cv.Optional("data_type", default="uint16"): cv.string,
-    }
-).extend(uart.UART_DEVICE_SCHEMA)
+# C++-klasser
+CustomModbus = custommodbus_ns.class_("CustomModbus", cg.Component, cg.UARTDevice)
+DataType = custommodbus_ns.enum("DataType")
 
+# Map mellem YAML string og C++ DataType enum
 DATA_TYPE_MAP = {
-    "uint16": "TYPE_UINT16",
-    "int16": "TYPE_INT16",
-    "uint32": "TYPE_UINT32",
-    "uint32_r": "TYPE_UINT32_R",
+    "uint16": DataType.TYPE_UINT16,
+    "int16": DataType.TYPE_INT16,
+    "uint32": DataType.TYPE_UINT32,
+    "uint32_r": DataType.TYPE_UINT32_R,
 }
 
+# Konfigurationsskema for en custommodbus-sensor
+CONFIG_SCHEMA = sensor.sensor_schema().extend(
+    {
+        cv.GenerateID(): cv.declare_id(sensor.Sensor),
+        cv.GenerateID("custommodbus_id"): cv.use_id(CustomModbus),
+        cv.Required(CONF_REGISTER): cv.uint16_t,
+        cv.Optional(CONF_COUNT, default=1): cv.one_of(1, 2, int=True),
+        cv.Optional(CONF_DATA_TYPE, default="uint16"): cv.one_of(
+            "uint16", "int16", "uint32", "uint32_r", lower=True
+        ),
+        cv.Optional(CONF_SCALE, default=1.0): cv.float_,
+    }
+)
 
 async def to_code(config):
+    # Hent reference til CustomModbus-objektet (parent)
     parent = await cg.get_variable(config["custommodbus_id"])
+
+    # Opret selve sensor-objektet
     sens = await sensor.new_sensor(config)
 
-    # Map string → enum symbol
-    data_type = DATA_TYPE_MAP.get(config["data_type"], "TYPE_UINT16")
+    # Find C++ enum-værdien til data_type
+    data_type = DATA_TYPE_MAP[config[CONF_DATA_TYPE]]
 
-    cg.add(parent.add_read_sensor(
-        config["register"],
-        config["count"],
-        custommodbus_ns.enum(data_type),
-        config["scale"],
-        sens
-    ))
-
-
-CONFIG_SCHEMA = PLATFORM_SCHEMA
+    # Tilføj læsning til CustomModbus
+    # Dette registrerer kun læsningen (ReadItem)
+    # Den reelle Modbus-trafik sker inde i C++ i loop().
+    cg.add(
+        parent.add_read_sensor(
+            config[CONF_REGISTER],
+            config[CONF_COUNT],
+            data_type,
+            config[CONF_SCALE],
+            sens,
+        )
+    )
