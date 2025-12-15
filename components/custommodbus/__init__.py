340b295bb1575cab032ac90e4bce8459256c9131
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-15 18:31:37 +0100

    Update __init__.py

--- components/custommodbus/__init__.py
+++ components/custommodbus/__init__.py
@@ -1,15 +1,27 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome.components import uart
+from esphome.const import CONF_ID
 
 custommodbus_ns = cg.esphome_ns.namespace("custommodbus")
-CustomModbus = custommodbus_ns.class_("CustomModbus", cg.Component)
+CustomModbus = custommodbus_ns.class_("CustomModbus", cg.Component, uart.UARTDevice)
 
 AUTO_LOAD = ["sensor", "binary_sensor", "switch", "number", "select", "text_sensor"]
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(CustomModbus),
-})
+CONF_SLAVE_ID = "slave_id"
+CONF_UART_ID = "uart_id"
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(CONF_ID): cv.declare_id(CustomModbus),
+        cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
+        cv.Required(CONF_SLAVE_ID): cv.uint8_t,
+    }
+)
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[cv.CONF_ID])
+    var = cg.new_Pvariable(config[CONF_ID])
+    uart_component = await cg.get_variable(config[CONF_UART_ID])
+    cg.add(var.set_parent(uart_component))
+    cg.add(var.set_slave_id(config[CONF_SLAVE_ID]))
     await cg.register_component(var, config)
