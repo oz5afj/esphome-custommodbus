d3490b2e19da5d0f298f50f0ccd598c723ba03e9
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-15 21:10:09 +0100

    Update custommodbus.py

--- components/custommodbus/custommodbus.py
+++ components/custommodbus/custommodbus.py
@@ -1,38 +1,25 @@
-#
-#  CustomModbus – hovedkomponent (ESPHome 2025+ kompatibel)
-#
-
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart
 from esphome.const import CONF_ID
 
-# C++ namespace
 custommodbus_ns = cg.esphome_ns.namespace("custommodbus")
-
-# C++ klasse
 CustomModbus = custommodbus_ns.class_("CustomModbus", cg.Component, uart.UARTDevice)
 
-# YAML felter
 CONF_SLAVE_ID = "slave_id"
 CONF_UART_ID = "uart_id"
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(CONF_ID): cv.declare_id(CustomModbus),
-
-        # Reference til UART komponent
         cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
-
-        # Modbus slave ID
         cv.Required(CONF_SLAVE_ID): cv.uint8_t,
     }
 )
+
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    uart_component = await cg.get_variable(config["uart_id"])
-    # Bind UART korrekt til C++ klassen
+    uart_component = await cg.get_variable(config[CONF_UART_ID])
     cg.add(var.set_uart_parent(uart_component))
-    # Sæt slave id (hvis din C++ klasse har denne metode)
-    cg.add(var.set_slave_id(config["slave_id"]))
+    cg.add(var.set_slave_id(config[CONF_SLAVE_ID]))
     await cg.register_component(var, config)
