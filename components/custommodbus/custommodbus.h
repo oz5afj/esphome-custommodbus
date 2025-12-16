4b161ea06d8a57c2fa4cb2884fadfa29b6d09e0c
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-16 15:52:10 +0100

    Update custommodbus.h

--- components/custommodbus/custommodbus.h
+++ components/custommodbus/custommodbus.h
@@ -1,18 +1,22 @@
 #pragma once
 
-// Grundlæggende ESPHome includes
 #include "esphome.h"
-
-// Vigtige platform-headers som vi bruger i både header og cpp
 #include "esphome/components/uart/uart.h"
 #include "esphome/components/sensor/sensor.h"
-#include "esphome/components/binary_sensor/binary_sensor.h"
-#include "esphome/components/text_sensor/text_sensor.h"
+
+// Forward declarations for binary/text sensors to avoid requiring their headers here
+namespace esphome {
+namespace binary_sensor {
+class BinarySensor;
+}
+namespace text_sensor {
+class TextSensor;
+}
+}  // namespace esphome
 
 namespace esphome {
 namespace custommodbus {
 
-// DataType enum (skal matche hvad Python sender)
 enum DataType : uint8_t {
   TYPE_UINT16 = 0,
   TYPE_INT16 = 1,
@@ -20,7 +24,6 @@ enum DataType : uint8_t {
   TYPE_UINT32_R = 3,
 };
 
-// Struktur til read-registreringer
 struct ReadItem {
   uint16_t reg;
   uint8_t count;
@@ -32,7 +35,6 @@ struct ReadItem {
   uint16_t bitmask;
 };
 
-// Struktur til write-kø
 struct WriteItem {
   uint16_t reg;
   uint16_t value;
@@ -44,11 +46,9 @@ class CustomModbus : public Component, public UARTDevice {
  public:
   CustomModbus() = default;
 
-  // Binding fra Python
   void set_uart_parent(UARTComponent *parent) { this->uart_parent_ = parent; }
   void set_slave_id(uint8_t id) { this->slave_id_ = id; }
 
-  // API som Python kalder
   void add_read_sensor(uint16_t reg, uint8_t count, DataType type, float scale, esphome::sensor::Sensor *s);
   void add_binary_sensor(uint16_t reg, uint16_t mask, esphome::binary_sensor::BinarySensor *bs);
   void add_text_sensor(uint16_t reg, esphome::text_sensor::TextSensor *ts);
@@ -56,12 +56,10 @@ class CustomModbus : public Component, public UARTDevice {
   void write_single(uint16_t reg, uint16_t value);
   void write_bitmask(uint16_t reg, uint16_t mask, bool state);
 
-  // Lifecycle
   void setup() override;
   void loop() override;
 
  protected:
-  // Internals
   void process_reads();
   void process_writes();
   bool read_registers(uint16_t reg, uint8_t count, uint8_t *resp, uint8_t &resp_len);
