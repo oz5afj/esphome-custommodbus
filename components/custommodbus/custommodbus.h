eb007167d387d8c11b5543d061ee8682e190872c
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-16 15:25:26 +0100

    Update custommodbus.h

--- components/custommodbus/custommodbus.h
+++ components/custommodbus/custommodbus.h
@@ -2,14 +2,25 @@
 
 #include "esphome.h"
 #include "esphome/components/uart/uart.h"
-#include "esphome/components/sensor/sensor.h"
-#include "esphome/components/binary_sensor/binary_sensor.h"
-#include "esphome/components/text_sensor/text_sensor.h"
+
+// Forward declarations for platform types to avoid direct includes in header
+namespace esphome {
+namespace sensor {
+class Sensor;
+}  // namespace sensor
+
+namespace binary_sensor {
+class BinarySensor;
+}  // namespace binary_sensor
+
+namespace text_sensor {
+class TextSensor;
+}  // namespace text_sensor
+}  // namespace esphome
 
 namespace esphome {
 namespace custommodbus {
 
-// DataType enum (skal matche hvad sensor.py sender)
 enum DataType : uint8_t {
   TYPE_UINT16 = 0,
   TYPE_INT16 = 1,
@@ -39,11 +50,9 @@ class CustomModbus : public Component, public UARTDevice {
  public:
   CustomModbus() = default;
 
-  // Binding fra Python
   void set_uart_parent(UARTComponent *parent) { this->uart_parent_ = parent; }
   void set_slave_id(uint8_t id) { this->slave_id_ = id; }
 
-  // API som Python kalder
   void add_read_sensor(uint16_t reg, uint8_t count, DataType type, float scale, esphome::sensor::Sensor *s);
   void add_binary_sensor(uint16_t reg, uint16_t mask, esphome::binary_sensor::BinarySensor *bs);
   void add_text_sensor(uint16_t reg, esphome::text_sensor::TextSensor *ts);
@@ -51,12 +60,10 @@ class CustomModbus : public Component, public UARTDevice {
   void write_single(uint16_t reg, uint16_t value);
   void write_bitmask(uint16_t reg, uint16_t mask, bool state);
 
-  // Lifecycle
   void setup() override;
   void loop() override;
 
  protected:
-  // Internals
   void process_reads();
   void process_writes();
   bool read_registers(uint16_t reg, uint8_t count, uint8_t *resp, uint8_t &resp_len);
