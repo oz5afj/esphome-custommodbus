153f8739273148ae1883222d9074ff285afc6d34
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-17 20:53:25 +0100

    Update __init__.py version 3

--- components/custommodbus/__init__.py
+++ components/custommodbus/__init__.py
@@ -1,25 +1,53 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import uart
-from esphome.const import CONF_ID
+from esphome.components import uart, sensor
+from esphome.const import CONF_ID, CONF_NAME
 
-custommodbus_ns = cg.esphome_ns.namespace("custommodbus")
-CustomModbus = custommodbus_ns.class_("CustomModbus", cg.Component, uart.UARTDevice)
+DEPENDENCIES = ['uart']
 
-CONF_SLAVE_ID = "slave_id"
-CONF_UART_ID = "uart_id"
+custommodbus_ns = cg.esphome_ns.namespace('custommodbus')
+CustomModbus = custommodbus_ns.class_('CustomModbus', cg.Component, uart.UARTDevice)
 
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(CONF_ID): cv.declare_id(CustomModbus),
-        cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
-        cv.Required(CONF_SLAVE_ID): cv.uint8_t,
-    }
-)
+CONF_SLAVE_ID = 'slave_id'
+CONF_UART_ID = 'uart_id'
+CONF_GROUPS = 'groups'
+
+READ_SCHEMA = cv.Schema({
+    cv.Required('reg'): cv.uint16_t,
+    cv.Required('count'): cv.uint8_t,
+    cv.Required(CONF_NAME): cv.string,
+    cv.Optional('smoothing_alpha', default=0.0): cv.float_,
+})
+
+GROUP_SCHEMA = cv.Schema({
+    cv.Required('name'): cv.string,
+    cv.Required('interval'): cv.positive_time_period_milliseconds,
+    cv.Required('reads'): cv.ensure_list(READ_SCHEMA),
+})
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(CONF_ID): cv.declare_id(CustomModbus),
+    cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
+    cv.Required(CONF_SLAVE_ID): cv.uint8_t,
+    cv.Required(CONF_GROUPS): cv.ensure_list(GROUP_SCHEMA),
+}, extra=cv.ALLOW_EXTRA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    uart_component = await cg.get_variable(config[CONF_UART_ID])
-    cg.add(var.set_uart_parent(uart_component))
+    uart_comp = await cg.get_variable(config[CONF_UART_ID])
+    cg.add(var.set_uart_parent(uart_comp))
     cg.add(var.set_slave_id(config[CONF_SLAVE_ID]))
     await cg.register_component(var, config)
+
+    # Opret grupper og reads
+    for g in config[CONF_GROUPS]:
+        group_name = g['name']
+        interval_ms = int(g['interval'])
+        cg.add(var.add_group(group_name, interval_ms))
+        for r in g['reads']:
+            # Opret en ESPHome sensor og registrer den
+            sens = sensor.new_sensor(r[CONF_NAME])
+            await sensor.register_sensor(sens, r[CONF_NAME])
+            # Tilføj read til gruppen (smoothing_alpha er valgfri)
+            alpha = float(r.get('smoothing_alpha', 0.0))
+            cg.add(var.add_read_to_group(group_name, r['reg'], r['count'], sens, alpha))
