9deb927db364a6b62567f6a85f7aac96f20e5e98
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-18 15:32:07 +0100

    Update number.py

--- components/custommodbus/number.py
+++ components/custommodbus/number.py
@@ -1,7 +1,14 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart, number
-from esphome.const import CONF_ID
+from esphome.const import (
+    CONF_ID,
+    CONF_DISABLED_BY_DEFAULT,
+    CONF_RESTORE_MODE,
+    CONF_ICON,
+    CONF_ENTITY_CATEGORY,
+    CONF_DEVICE_CLASS,
+)
 
 custommodbus_ns = cg.esphome_ns.namespace("custommodbus")
 CustomModbus = custommodbus_ns.class_("CustomModbus", cg.Component, uart.UARTDevice)
@@ -17,8 +24,13 @@ PLATFORM_SCHEMA = cv.Schema(
         cv.Optional("max_value"): cv.float_,
         cv.Optional("step"): cv.float_,
         cv.Optional("unit_of_measurement"): cv.string,
+        cv.Optional(CONF_ICON): cv.icon,
+        cv.Optional(CONF_ENTITY_CATEGORY): cv.string,
+        cv.Optional(CONF_DEVICE_CLASS): cv.string,
+        cv.Optional(CONF_DISABLED_BY_DEFAULT, default=False): cv.boolean,
+        cv.Optional(CONF_RESTORE_MODE, default="RESTORE_DEFAULT"): cv.string,
     }
-).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
+).extend(uart.UART_DEVICE_SCHEMA)
 
 CONFIG_SCHEMA = PLATFORM_SCHEMA
 
@@ -26,10 +38,12 @@ CONFIG_SCHEMA = PLATFORM_SCHEMA
 async def to_code(config):
     parent = await cg.get_variable(config["custommodbus_id"])
     await uart.register_uart_device(parent, config)
+
+    config.setdefault(CONF_DISABLED_BY_DEFAULT, False)
+    config.setdefault(CONF_RESTORE_MODE, "RESTORE_DEFAULT")
+
     num = await number.new_number(config)
 
     cg.add(parent.set_slave_id(config["slave_id"]))
 
-    cg.add(num.add_on_state_callback(
-        parent.write_single(config["register"], cg.float_(num))
-    ))
+    # Hvis du senere vil skrive værdier fra number til Modbus, kan du tilføje callbacks i parent-komponenten.
