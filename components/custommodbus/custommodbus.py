d90a93b14294583573707ac591a5bce0423f0a19
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-18 21:15:14 +0100

    Update custommodbus.py

--- components/custommodbus/custommodbus.py
+++ components/custommodbus/custommodbus.py
@@ -1,36 +1,31 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_UART_ID
 
 custommodbus_ns = cg.esphome_ns.namespace("custommodbus")
 CustomModbus = custommodbus_ns.class_("CustomModbus", cg.Component, uart.UARTDevice)
 
 CONF_SLAVE_ID = "slave_id"
-CONF_UART_ID = "uart_id"
 CONF_USE_GROUPED_READS = "use_grouped_reads"
 
 CONFIG_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(CONF_ID): cv.declare_id(CustomModbus),
+        cv.GenerateID(): cv.declare_id(CustomModbus),
         cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
-        cv.Required(CONF_SLAVE_ID): cv.uint8_t,
+        cv.Required(CONF_SLAVE_ID): cv.int_range(min=1, max=247),
         cv.Optional(CONF_USE_GROUPED_READS, default=False): cv.boolean,
     }
 )
 
-async def to_code(config):
+def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-
-    # Bind UART parent
-    uart_component = await cg.get_variable(config[CONF_UART_ID])
+    uart_component = yield cg.get_variable(config[CONF_UART_ID])
     cg.add(var.set_uart_parent(uart_component))
 
-    # Slave ID
     cg.add(var.set_slave_id(config[CONF_SLAVE_ID]))
 
-    # Grouped reads (valgfrit)
-    cg.add(var.set_use_grouped_reads(config[CONF_USE_GROUPED_READS]))
+    if CONF_USE_GROUPED_READS in config:
+        cg.add(var.set_use_grouped_reads(config[CONF_USE_GROUPED_READS]))
 
-    # Register component
-    await cg.register_component(var, config)
+    cg.add(var.setup())
