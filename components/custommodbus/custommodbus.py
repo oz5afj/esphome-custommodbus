9ae3b138c256949ff468139d8be7d597b9bc45fc
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-18 21:49:25 +0100

    Update custommodbus.py

--- components/custommodbus/custommodbus.py
+++ components/custommodbus/custommodbus.py
@@ -3,15 +3,12 @@ import esphome.config_validation as cv
 from esphome.components import uart
 from esphome.const import CONF_ID, CONF_UART_ID
 
-# Namespace
 custommodbus_ns = cg.esphome_ns.namespace("custommodbus")
 CustomModbus = custommodbus_ns.class_("CustomModbus", cg.Component, uart.UARTDevice)
 
-# Configuration keys
 CONF_SLAVE_ID = "slave_id"
 CONF_USE_GROUPED_READS = "use_grouped_reads"
 
-# Configuration schema
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(CONF_ID): cv.declare_id(CustomModbus),
@@ -21,19 +18,13 @@ CONFIG_SCHEMA = cv.Schema(
     }
 )
 
-
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-
-    # Register component so schema + YAML options bind correctly
     cg.register_component(var, config)
 
-    # Bind UART parent
     uart_component = yield cg.get_variable(config[CONF_UART_ID])
     cg.add(var.set_uart_parent(uart_component))
 
-    # Slave ID
     cg.add(var.set_slave_id(config[CONF_SLAVE_ID]))
 
-    # Grouped reads (always present because of default)
     cg.add(var.set_use_grouped_reads(config[CONF_USE_GROUPED_READS]))
