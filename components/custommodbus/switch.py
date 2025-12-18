5fc32466666081191e66b622660909728ca7ea60
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-18 15:31:18 +0100

    Update switch.py

--- components/custommodbus/switch.py
+++ components/custommodbus/switch.py
@@ -1,7 +1,14 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart, switch
-from esphome.const import CONF_ID, CONF_DISABLED_BY_DEFAULT, CONF_ICON, CONF_ENTITY_CATEGORY, CONF_DEVICE_CLASS
+from esphome.const import (
+    CONF_ID,
+    CONF_DISABLED_BY_DEFAULT,
+    CONF_RESTORE_MODE,
+    CONF_ICON,
+    CONF_ENTITY_CATEGORY,
+    CONF_DEVICE_CLASS,
+)
 
 custommodbus_ns = cg.esphome_ns.namespace("custommodbus")
 CustomModbus = custommodbus_ns.class_("CustomModbus", cg.Component, uart.UARTDevice)
@@ -14,12 +21,11 @@ PLATFORM_SCHEMA = cv.Schema(
         cv.Required("slave_id"): cv.int_range(min=1, max=247),
         cv.Required("register"): cv.hex_uint16_t,
         cv.Optional("bitmask"): cv.hex_uint16_t,
-
-        # Entity fields added explicitly (robust over forskellige ESPHome versioner)
         cv.Optional(CONF_ICON): cv.icon,
         cv.Optional(CONF_ENTITY_CATEGORY): cv.string,
         cv.Optional(CONF_DEVICE_CLASS): cv.string,
         cv.Optional(CONF_DISABLED_BY_DEFAULT, default=False): cv.boolean,
+        cv.Optional(CONF_RESTORE_MODE, default="RESTORE_DEFAULT"): cv.string,
     }
 ).extend(uart.UART_DEVICE_SCHEMA)
 
@@ -30,8 +36,9 @@ async def to_code(config):
     parent = await cg.get_variable(config["custommodbus_id"])
     await uart.register_uart_device(parent, config)
 
-    # Sikr default så setup_entity ikke kaster KeyError
+    # sikre defaults så core ikke kaster KeyError
     config.setdefault(CONF_DISABLED_BY_DEFAULT, False)
+    config.setdefault(CONF_RESTORE_MODE, "RESTORE_DEFAULT")
 
     sw = await switch.new_switch(config)
 
