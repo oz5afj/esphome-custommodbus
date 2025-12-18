d910be64d6e802b7cc739588b4827a1956bdc7c0
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-18 07:49:45 +0100

    Update number.py

--- components/custommodbus/number.py
+++ components/custommodbus/number.py
@@ -6,12 +6,11 @@ from esphome.const import CONF_ID
 custommodbus_ns = cg.esphome_ns.namespace("custommodbus")
 CustomModbus = custommodbus_ns.class_("CustomModbus", cg.Component, uart.UARTDevice)
 
-# Robust schema defineret direkte med cv.Schema
 PLATFORM_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(number.Number),
         cv.Optional("name"): cv.string,
-        cv.Required(CONF_ID): cv.use_id(CustomModbus),
+        cv.Required("custommodbus_id"): cv.use_id(CustomModbus),
         cv.Required("slave_id"): cv.int_range(min=1, max=247),
         cv.Required("register"): cv.hex_uint16_t,
         cv.Optional("min_value"): cv.float_,
@@ -21,19 +20,16 @@ PLATFORM_SCHEMA = cv.Schema(
     }
 ).extend(uart.UART_DEVICE_SCHEMA)
 
-# ESPHome loader forventer CONFIG_SCHEMA
 CONFIG_SCHEMA = PLATFORM_SCHEMA
 
 
 async def to_code(config):
-    parent = await cg.get_variable(config[CONF_ID])
+    parent = await cg.get_variable(config["custommodbus_id"])
     await uart.register_uart_device(parent, config)
     num = await number.new_number(config)
 
     cg.add(parent.set_slave_id(config["slave_id"]))
 
-    # Tilknyt callback: skriv register ved ændring af talværdien.
-    # Bemærk: parent.write_single forventer et tal; cg.float_(num) bruges som wrapper til codegen.
     cg.add(num.add_on_state_callback(
         parent.write_single(config["register"], cg.float_(num))
     ))
