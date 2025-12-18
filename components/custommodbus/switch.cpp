34aba3599f39bbbf37fbe8b529dbf1ad8f7373aa
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-18 17:01:21 +0100

    Update switch.cpp

--- components/custommodbus/switch.cpp
+++ components/custommodbus/switch.cpp
@@ -1,9 +1,17 @@
-#include "custommodbus_switch.h"
+#include "switch.h"
+#include "custommodbus.h"
 
 namespace esphome {
 namespace custommodbus {
 
-// Ingen ekstra logik nødvendig – alt håndteres i write_state()
+void CustomModbusSwitch::write_state(bool state) {
+  if (bitmask_ != 0) {
+    parent_->write_bitmask(register_, bitmask_, state);
+  } else {
+    parent_->write_single(register_, state ? 1 : 0);
+  }
+  publish_state(state);
+}
 
 }  // namespace custommodbus
 }  // namespace esphome
