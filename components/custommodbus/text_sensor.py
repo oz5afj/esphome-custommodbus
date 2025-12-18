6c62745906add7313604ac2e44c52218d403022b
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-18 15:32:56 +0100

    Update text_sensor.py

--- components/custommodbus/text_sensor.py
+++ components/custommodbus/text_sensor.py
@@ -1,7 +1,14 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart, text_sensor
-from esphome.const import CONF_ID
+from esphome.const import (
+    CONF_ID,
+    CONF_DISABLED_BY_DEFAULT,
+    CONF_RESTORE_MODE,
+    CONF_ICON,
+    CONF_ENTITY_CATEGORY,
+    CONF_DEVICE_CLASS,
+)
 
 custommodbus_ns = cg.esphome_ns.namespace("custommodbus")
 CustomModbus = custommodbus_ns.class_("CustomModbus", cg.Component, uart.UARTDevice)
@@ -15,9 +22,13 @@ PLATFORM_SCHEMA = cv.Schema(
         cv.Required("register"): cv.hex_uint16_t,
         cv.Optional("data_type"): cv.string,
         cv.Optional("raw_encode"): cv.string,
-        cv.Optional("icon"): cv.icon,
+        cv.Optional(CONF_ICON): cv.icon,
+        cv.Optional(CONF_ENTITY_CATEGORY): cv.string,
+        cv.Optional(CONF_DEVICE_CLASS): cv.string,
+        cv.Optional(CONF_DISABLED_BY_DEFAULT, default=False): cv.boolean,
+        cv.Optional(CONF_RESTORE_MODE, default="RESTORE_DEFAULT"): cv.string,
     }
-).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
+).extend(uart.UART_DEVICE_SCHEMA)
 
 CONFIG_SCHEMA = PLATFORM_SCHEMA
 
@@ -25,10 +36,11 @@ CONFIG_SCHEMA = PLATFORM_SCHEMA
 async def to_code(config):
     parent = await cg.get_variable(config["custommodbus_id"])
     await uart.register_uart_device(parent, config)
+
+    config.setdefault(CONF_DISABLED_BY_DEFAULT, False)
+    config.setdefault(CONF_RESTORE_MODE, "RESTORE_DEFAULT")
+
     ts = await text_sensor.new_text_sensor(config)
 
     cg.add(parent.set_slave_id(config["slave_id"]))
-    cg.add(parent.add_text_sensor(
-        config["register"],
-        ts
-    ))
+    cg.add(parent.add_text_sensor(config["register"], ts))
