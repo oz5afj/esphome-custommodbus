733a2360356f2da9255b7ecc8efb85fce841e894
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-18 19:25:44 +0100

    Update sensor.py

--- components/custommodbus/sensor.py
+++ components/custommodbus/sensor.py
@@ -1,11 +1,13 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
+from esphome.const import CONF_ID
 
 CONF_REGISTER = "register"
 CONF_COUNT = "count"
 CONF_DATA_TYPE = "data_type"
 CONF_SCALE = "scale"
+CONF_CUSTOMMODBUS_ID = "custommodbus_id"
 
 custommodbus_ns = cg.esphome_ns.namespace("custommodbus")
 CustomModbus = custommodbus_ns.class_("CustomModbus", cg.Component)
@@ -20,7 +22,8 @@ DATA_TYPE_MAP = {
 CONFIG_SCHEMA = sensor.sensor_schema().extend(
     {
         cv.GenerateID(): cv.declare_id(sensor.Sensor),
-        cv.GenerateID("custommodbus_id"): cv.use_id(CustomModbus),
+        cv.GenerateID(CONF_CUSTOMMODBUS_ID): cv.use_id(CustomModbus),
+
         cv.Required(CONF_REGISTER): cv.uint16_t,
         cv.Optional(CONF_COUNT, default=1): cv.one_of(1, 2, int=True),
         cv.Optional(CONF_DATA_TYPE, default="uint16"): cv.one_of(
@@ -31,7 +34,15 @@ CONFIG_SCHEMA = sensor.sensor_schema().extend(
 )
 
 async def to_code(config):
-    parent = await cg.get_variable(config["custommodbus_id"])
+    parent = await cg.get_variable(config[CONF_CUSTOMMODBUS_ID])
     sens = await sensor.new_sensor(config)
+
     data_type = DATA_TYPE_MAP[config[CONF_DATA_TYPE]]
-    cg.add(parent.add_read_sensor(config[CONF_REGISTER], config[CONF_COUNT], data_type, config[CONF_SCALE], sens))
+
+    cg.add(parent.add_read_sensor(
+        config[CONF_REGISTER],
+        config[CONF_COUNT],
+        data_type,
+        config[CONF_SCALE],
+        sens
+    ))
