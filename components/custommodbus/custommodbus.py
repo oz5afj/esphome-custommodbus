9a9cb081afd3041fb4e7a5ef63954b7c95f2dc87
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-18 19:49:29 +0100

    Update custommodbus.py G-read

--- components/custommodbus/custommodbus.py
+++ components/custommodbus/custommodbus.py
@@ -8,18 +8,29 @@ CustomModbus = custommodbus_ns.class_("CustomModbus", cg.Component, uart.UARTDev
 
 CONF_SLAVE_ID = "slave_id"
 CONF_UART_ID = "uart_id"
+CONF_USE_GROUPED_READS = "use_grouped_reads"
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(CONF_ID): cv.declare_id(CustomModbus),
         cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
         cv.Required(CONF_SLAVE_ID): cv.uint8_t,
+        cv.Optional(CONF_USE_GROUPED_READS, default=False): cv.boolean,
     }
 )
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
+
+    # Bind UART parent
     uart_component = await cg.get_variable(config[CONF_UART_ID])
     cg.add(var.set_uart_parent(uart_component))
+
+    # Slave ID
     cg.add(var.set_slave_id(config[CONF_SLAVE_ID]))
+
+    # Grouped reads (valgfrit)
+    cg.add(var.set_use_grouped_reads(config[CONF_USE_GROUPED_READS]))
+
+    # Register component
     await cg.register_component(var, config)
