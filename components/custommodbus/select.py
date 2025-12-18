87c7c45dce5768fe72564ee917db777c5c2a302a
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-18 15:32:24 +0100

    Update select.py

--- components/custommodbus/select.py
+++ components/custommodbus/select.py
@@ -1,7 +1,14 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart, select
-from esphome.const import CONF_ID
+from esphome.const import (
+    CONF_ID,
+    CONF_DISABLED_BY_DEFAULT,
+    CONF_RESTORE_MODE,
+    CONF_ICON,
+    CONF_ENTITY_CATEGORY,
+    CONF_DEVICE_CLASS,
+)
 
 custommodbus_ns = cg.esphome_ns.namespace("custommodbus")
 CustomModbus = custommodbus_ns.class_("CustomModbus", cg.Component, uart.UARTDevice)
@@ -14,9 +21,13 @@ PLATFORM_SCHEMA = cv.Schema(
         cv.Required("slave_id"): cv.int_range(min=1, max=247),
         cv.Required("register"): cv.hex_uint16_t,
         cv.Required("options"): cv.Schema({cv.string: cv.positive_int}),
-        cv.Optional("icon"): cv.icon,
+        cv.Optional(CONF_ICON): cv.icon,
+        cv.Optional(CONF_ENTITY_CATEGORY): cv.string,
+        cv.Optional(CONF_DEVICE_CLASS): cv.string,
+        cv.Optional(CONF_DISABLED_BY_DEFAULT, default=False): cv.boolean,
+        cv.Optional(CONF_RESTORE_MODE, default="RESTORE_DEFAULT"): cv.string,
     }
-).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
+).extend(uart.UART_DEVICE_SCHEMA)
 
 CONFIG_SCHEMA = PLATFORM_SCHEMA
 
@@ -24,13 +35,14 @@ CONFIG_SCHEMA = PLATFORM_SCHEMA
 async def to_code(config):
     parent = await cg.get_variable(config["custommodbus_id"])
     await uart.register_uart_device(parent, config)
+
+    config.setdefault(CONF_DISABLED_BY_DEFAULT, False)
+    config.setdefault(CONF_RESTORE_MODE, "RESTORE_DEFAULT")
+
     sel = await select.new_select(config)
 
     cg.add(parent.set_slave_id(config["slave_id"]))
 
-    for name, value in config["options"].items():
+    # options registreres via sel.add_option i to_code hvis nødvendigt i din komponent
+    for name, value in config.get("options", {}).items():
         cg.add(sel.add_option(name, value))
-
-    cg.add(sel.add_on_state_callback(
-        parent.write_single(config["register"], cg.uint16_t(sel))
-    ))
