f194aa62e689eb54f1495996fa44d9bc1de1cbcb
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-18 22:11:54 +0100

    Update __init__.py

--- components/custommodbus/__init__.py
+++ components/custommodbus/__init__.py
@@ -1,8 +1,10 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from .custommodbus import *
+
 from esphome.components import uart
 from esphome.const import CONF_ID
-from .custommodbus import *
+
 
 custommodbus_ns = cg.esphome_ns.namespace("custommodbus")
 CustomModbus = custommodbus_ns.class_("CustomModbus", cg.Component, uart.UARTDevice)
