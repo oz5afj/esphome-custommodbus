cd646eed41cffcfad2d3f75d5b7dc19c83f5ed33
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-18 15:32:40 +0100

    Update binary_sensor.py

--- components/custommodbus/binary_sensor.py
+++ components/custommodbus/binary_sensor.py
@@ -1,7 +1,14 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart, binary_sensor
-from esphome.const import CONF_ID
+from esphome.const import (
+    CONF_ID,
+    CONF_DISABLED_BY_DEFAULT,
+    CONF_RESTORE_MODE,
+    CONF_ICON,
+    CONF_ENTITY_CATEGORY,
+    CONF_DEVICE_CLASS,
+)
 
 custommodbus_ns = cg.esphome_ns.namespace("custommodbus")
 CustomModbus = custommodbus_ns.class_("CustomModbus", cg.Component, uart.UARTDevice)
@@ -14,10 +21,13 @@ PLATFORM_SCHEMA = cv.Schema(
         cv.Required("slave_id"): cv.int_range(min=1, max=247),
         cv.Required("register"): cv.hex_uint16_t,
         cv.Required("bitmask"): cv.hex_uint16_t,
-        cv.Optional("device_class"): cv.string,
-        cv.Optional("icon"): cv.icon,
+        cv.Optional(CONF_ICON): cv.icon,
+        cv.Optional(CONF_ENTITY_CATEGORY): cv.string,
+        cv.Optional(CONF_DEVICE_CLASS): cv.string,
+        cv.Optional(CONF_DISABLED_BY_DEFAULT, default=False): cv.boolean,
+        cv.Optional(CONF_RESTORE_MODE, default="RESTORE_DEFAULT"): cv.string,
     }
-).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
+).extend(uart.UART_DEVICE_SCHEMA)
 
 CONFIG_SCHEMA = PLATFORM_SCHEMA
 
@@ -25,11 +35,11 @@ CONFIG_SCHEMA = PLATFORM_SCHEMA
 async def to_code(config):
     parent = await cg.get_variable(config["custommodbus_id"])
     await uart.register_uart_device(parent, config)
+
+    config.setdefault(CONF_DISABLED_BY_DEFAULT, False)
+    config.setdefault(CONF_RESTORE_MODE, "RESTORE_DEFAULT")
+
     bs = await binary_sensor.new_binary_sensor(config)
 
     cg.add(parent.set_slave_id(config["slave_id"]))
-    cg.add(parent.add_binary_sensor(
-        config["register"],
-        config["bitmask"],
-        bs
-    ))
+    cg.add(parent.add_binary_sensor(config["register"], config["bitmask"], bs))
