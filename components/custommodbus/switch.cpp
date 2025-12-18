57ae96e0310fd168336559ae51877a6f9862f7c1
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-18 16:53:19 +0100

    Create switch.cpp

---
+++ components/custommodbus/switch.cpp
@@ -0,0 +1,9 @@
+#include "custommodbus_switch.h"
+
+namespace esphome {
+namespace custommodbus {
+
+// Ingen ekstra logik nødvendig – alt håndteres i write_state()
+
+}  // namespace custommodbus
+}  // namespace esphome
