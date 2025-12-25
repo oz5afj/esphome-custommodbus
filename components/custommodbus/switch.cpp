a8d158257ca0b002d67b3da378746a6ff3e69e68
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-25 21:07:17 +0100

    Update switch.cpp

--- components/custommodbus/switch.cpp
+++ components/custommodbus/switch.cpp
@@ -1,7 +1,6 @@
 #include "switch.h"
 #include "custommodbus.h"
 #include "esphome/core/log.h"
-#include "esphome/core/component.h"
 
 namespace esphome {
 namespace custommodbus {
@@ -9,18 +8,19 @@ namespace custommodbus {
 static const char *TAG = "custommodbus.switch";
 
 void CustomModbusSwitch::write_state(bool state) {
-  if (bitmask_ != 0) {
-    parent_->write_bitmask(register_, bitmask_, state);
-  } else {
-    parent_->write_single(register_, state ? 1 : 0);
+  if (this->parent_ == nullptr) {
+    ESP_LOGW(TAG, "No parent assigned for switch component");
+    return;
   }
 
-  // Update HA immediately
-  publish_state(state);
-}
+  if (this->bitmask_ != 0) {
+    this->parent_->write_bitmask(this->register_, this->bitmask_, state);
+  } else {
+    this->parent_->write_single(this->register_, state ? 1 : 0);
+  }
 
-// *** DENNE LINJE ER AFGØRENDE ***
-REGISTER_SWITCH(CustomModbusSwitch, switch_::Switch, "custommodbus");
+  this->publish_state(state);
+}
 
 }  // namespace custommodbus
 }  // namespace esphome
