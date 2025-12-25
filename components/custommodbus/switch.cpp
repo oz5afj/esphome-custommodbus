546e07395e5d93de2c367ff403bc656e1eeb882f
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-25 20:36:14 +0100

    Update switch.cpp

--- components/custommodbus/switch.cpp
+++ components/custommodbus/switch.cpp
@@ -1,8 +1,12 @@
-#include "switch.h"
+##include "switch.h"
 #include "custommodbus.h"
+#include "esphome/core/log.h"
+#include "esphome/core/component.h"
 
 namespace esphome {
 namespace custommodbus {
+
+static const char *TAG = "custommodbus.switch";
 
 void CustomModbusSwitch::write_state(bool state) {
   if (bitmask_ != 0) {
@@ -10,8 +14,13 @@ void CustomModbusSwitch::write_state(bool state) {
   } else {
     parent_->write_single(register_, state ? 1 : 0);
   }
+
+  // Update HA immediately
   publish_state(state);
 }
+
+// *** DENNE LINJE ER AFGØRENDE ***
+REGISTER_SWITCH(CustomModbusSwitch, switch_::Switch, "custommodbus");
 
 }  // namespace custommodbus
 }  // namespace esphome
