6d0acdbfd54fc041af2b68f056489b6a9dc17d2a
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-25 20:42:09 +0100

    Update number.cpp

--- components/custommodbus/number.cpp
+++ components/custommodbus/number.cpp
@@ -1,27 +1,35 @@
-#include "number.h"
-#include "custommodbus.h"
-#include "esphome/core/log.h"
-#include "esphome/core/component.h"
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import number
+from esphome.const import CONF_ID
 
-namespace esphome {
-namespace custommodbus {
+from . import custommodbus_ns, CustomModbus
 
-static const char *TAG = "custommodbus.number";
+CustomModbusNumber = custommodbus_ns.class_("CustomModbusNumber", number.Number)
 
-void CustomModbusNumber::control(float value) {
-  uint16_t val = static_cast<uint16_t>(value);
+CONF_REGISTER = "register"
+CONF_BITMASK = "bitmask"
+CONF_SLAVE_ID = "slave_id"
 
-  if (bitmask_ != 0) {
-    parent_->write_bitmask(register_, bitmask_, val != 0);
-  } else {
-    parent_->write_single(register_, val);
-  }
+CONFIG_SCHEMA = number.NUMBER_SCHEMA.extend(
+    {
+        cv.GenerateID(): cv.declare_id(CustomModbusNumber),
+        cv.Required(CONF_REGISTER): cv.uint16_t,
+        cv.Optional(CONF_BITMASK, default=0): cv.uint16_t,
+        cv.Optional(CONF_SLAVE_ID, default=1): cv.uint8_t,
+        cv.GenerateID("custommodbus_id"): cv.use_id(CustomModbus),
+    }
+)
 
-  publish_state(value);
-}
+async def to_code(config):
+    parent = await cg.get_variable(config["custommodbus_id"])
+    var = cg.new_Pvariable(config[CONF_ID])
 
-// *** KORREKT REGISTRERING ***
-REGISTER_NUMBER(CustomModbusNumber, number::Number, "custommodbus");
+    await number.register_number(var, config)
 
-}  // namespace custommodbus
-}  // namespace esphome
+    cg.add(var.set_parent(parent))
+    cg.add(var.set_register(config[CONF_REGISTER]))
+    cg.add(var.set_bitmask(config[CONF_BITMASK]))
+    cg.add(var.set_slave_id(config[CONF_SLAVE_ID]))
+
+    cg.add(parent.add_number(config[CONF_REGISTER], var))
