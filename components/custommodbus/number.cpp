c4a4792344ce9033ddde61127ccba4b7ee5d1a0d
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-25 20:34:11 +0100

    Update number.cpp

--- components/custommodbus/number.cpp
+++ components/custommodbus/number.cpp
@@ -1,21 +1,27 @@
 #include "number.h"
 #include "custommodbus.h"
+#include "esphome/core/log.h"
+#include "esphome/core/component.h"
 
 namespace esphome {
 namespace custommodbus {
+
+static const char *TAG = "custommodbus.number";
 
 void CustomModbusNumber::control(float value) {
   uint16_t val = static_cast<uint16_t>(value);
 
   if (bitmask_ != 0) {
-    parent_->write_bitmask(register_, bitmask_, val);
+    parent_->write_bitmask(register_, bitmask_, val != 0);
   } else {
     parent_->write_single(register_, val);
   }
 
   publish_state(value);
 }
-REGISTER_NUMBER(CustomModbusNumber, custommodbus, "custommodbus");
+
+// *** KORREKT REGISTRERING ***
+REGISTER_NUMBER(CustomModbusNumber, number::Number, "custommodbus");
 
 }  // namespace custommodbus
 }  // namespace esphome
