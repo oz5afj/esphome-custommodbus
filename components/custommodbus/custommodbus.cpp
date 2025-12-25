280d4610a84faad91452aebb2824f329934b3745
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-25 20:29:09 +0100

    Update custommodbus.cpp

--- components/custommodbus/custommodbus.cpp
+++ components/custommodbus/custommodbus.cpp
@@ -878,32 +878,12 @@ void CustomModbus::record_write(uint16_t reg, uint16_t value) {
 }
 
 
-void CustomModbusNumber::control(float value) {
-  if (!this->parent_) {
-    ESP_LOGW(TAG, "No parent set for number");
-    return;
-  }
-
-  uint16_t raw = static_cast<uint16_t>(value);
-
-  if (this->bitmask_ != 0) {
-    // Bitmask write (0x06 with read-modify-write)
-    this->parent_->write_bitmask(this->register_, this->bitmask_, raw != 0);
-  } else {
-    // Normal single register write
-    this->parent_->write_single(this->register_, raw);
-  }
-
-  // Update HA immediately
-  this->publish_state(value);
-}
-
-
 
 
 
 }  // namespace custommodbus
 }  // namespace esphome
+
 
 
 
