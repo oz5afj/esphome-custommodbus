e9390046228ea09312b3c9bd8e3cf87b2fca29f0
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-25 22:39:14 +0100

    Update switch.py

--- components/custommodbus/switch.py
+++ components/custommodbus/switch.py
@@ -1,7 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart, switch
-from esphome.components.switch import SwitchRestoreMode, restore_mode
 from esphome.const import (
     CONF_ID,
     CONF_NAME,
@@ -16,6 +15,13 @@ custommodbus_ns = cg.esphome_ns.namespace("custommodbus")
 CustomModbus = custommodbus_ns.class_("CustomModbus", cg.Component, uart.UARTDevice)
 CustomModbusSwitch = custommodbus_ns.class_("CustomModbusSwitch", switch.Switch)
 
+RESTORE_MODES = {
+    "RESTORE_DEFAULT_OFF": switch.SwitchRestoreMode.SWITCH_RESTORE_DEFAULT_OFF,
+    "RESTORE_DEFAULT_ON": switch.SwitchRestoreMode.SWITCH_RESTORE_DEFAULT_ON,
+    "ALWAYS_OFF": switch.SwitchRestoreMode.SWITCH_ALWAYS_OFF,
+    "ALWAYS_ON": switch.SwitchRestoreMode.SWITCH_ALWAYS_ON,
+}
+
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(CustomModbusSwitch),
@@ -23,29 +29,20 @@ CONFIG_SCHEMA = cv.Schema(
         cv.Required("slave_id"): cv.int_range(min=1, max=247),
         cv.Required("register"): cv.hex_uint16_t,
         cv.Optional("bitmask", default=0): cv.hex_uint16_t,
-
         cv.Optional(CONF_NAME): cv.string,
         cv.Optional(CONF_ICON): cv.icon,
         cv.Optional(CONF_ENTITY_CATEGORY): cv.string,
         cv.Optional(CONF_DEVICE_CLASS): cv.string,
         cv.Optional(CONF_DISABLED_BY_DEFAULT, default=False): cv.boolean,
-
-        # Brug ESPHome's egen restore_mode-validator og enum-type
-        cv.Optional(
-            CONF_RESTORE_MODE,
-            default=SwitchRestoreMode.SWITCH_RESTORE_DEFAULT_OFF,
-        ): restore_mode,
+        cv.Optional(CONF_RESTORE_MODE, default="RESTORE_DEFAULT_OFF"): cv.enum(RESTORE_MODES),
     }
 ).extend(uart.UART_DEVICE_SCHEMA)
-
 
 async def to_code(config):
     parent = await cg.get_variable(config["custommodbus_id"])
     await uart.register_uart_device(parent, config)
 
     sw = cg.new_Pvariable(config[CONF_ID])
-
-    # Nu får register_switch en rigtig enum, ikke en streng
     await switch.register_switch(sw, config)
 
     cg.add(sw.set_parent(parent))
