2a77ec7cefd338d8e0cfd107615ed2b8e2fe8757
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-25 20:03:42 +0100

    Update custommodbus.cpp

--- components/custommodbus/custommodbus.cpp
+++ components/custommodbus/custommodbus.cpp
@@ -404,7 +404,7 @@ void CustomModbus::setup() {
 
 void CustomModbus::loop() {
   // Throttle hele loopet: sørg for mindst X ms mellem iterationer der starter nye reads/writes
-  static const uint32_t LOOP_THROTTLE_MS = 2000; // 2 sekund mellem "aktive" iterationer
+  static const uint32_t LOOP_THROTTLE_MS = 250; // 2 sekund mellem "aktive" iterationer
   static uint32_t last_loop_ms = 0;
   uint32_t now = millis();
 
@@ -877,6 +877,7 @@ void CustomModbus::record_write(uint16_t reg, uint16_t value) {
 
 }  // namespace custommodbus
 }  // namespace esphome
+
 
 
 
