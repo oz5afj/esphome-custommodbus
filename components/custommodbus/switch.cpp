d3976d6f3e226e0ffe27f3e877c0df53b85ab430
oz5afj <58389425+oz5afj@users.noreply.github.com>
2025-12-25 21:31:56 +0100

    Update switch.cpp

--- components/custommodbus/switch.cpp
+++ components/custommodbus/switch.cpp
@@ -8,18 +8,18 @@ namespace custommodbus {
 static const char *TAG = "custommodbus.switch";
 
 void CustomModbusSwitch::write_state(bool state) {
-  if (this->parent_ == nullptr) {
-    ESP_LOGW(TAG, "No parent assigned for switch component");
+  if (!parent_) {
+    ESP_LOGW(TAG, "No parent assigned");
     return;
   }
 
-  if (this->bitmask_ != 0) {
-    this->parent_->write_bitmask(this->register_, this->bitmask_, state);
+  if (bitmask_ != 0) {
+    parent_->write_bitmask(register_, bitmask_, state);
   } else {
-    this->parent_->write_single(this->register_, state ? 1 : 0);
+    parent_->write_single(register_, state ? 1 : 0);
   }
 
-  this->publish_state(state);
+  publish_state(state);
 }
 
 }  // namespace custommodbus
